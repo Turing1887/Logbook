db80a8bc741aa3ca936e779dc088ff5a1c418ea6
Turing1887 <tim.zschage@t-online.de>
2016-09-06 15:25:21 +0200

    Upload root files

---
+++ main.js
@@ -0,0 +1,27 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+'use strict';
+
+const electron = require('electron');
+const path = require('path');
+
+
+
+// Module to control application life.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let win = null;
+
+app.on('ready', function() {
+    win = new BrowserWindow({
+        height: 600,
+        width: 800,
+        frame: false
+    });
+    win.loadURL('file://' + __dirname + '/index.html');
+});
+
+
+
