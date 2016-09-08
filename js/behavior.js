3677eb4c5ebc361c65d622cb764b909e813e147d
Turing1887 <tim.zschage@t-online.de>
2016-09-08 18:09:18 +0200

    Update behavior.js
    * create log file function added
    * append line to log file

--- js/behavior.js
+++ js/behavior.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+var openFile;
+var entry_note;
+var entry_num= 1;
+var mtime;
+var mtime_str;
+var entry_num_str = entry_num.toString();
+
 $(document).ready(function(){
-    $(".write-log").keydown(function(event){
-        if(event.which == 43){
-            createLogFile();
-        }
-    });
+    
     $(".add-btn").click(function(){
         $(".main").addClass("hidden");
         $(".file-screen").removeClass("hidden");
@@ -12,5 +16,47 @@ $(document).ready(function(){
         $(".main").removeClass("hidden");
         $(".file-screen").addClass("hidden");
     });
+
+
+    $("#file-form").submit(function(){
+		createLogFile();
+
+		$(".open-logname").html($("#filename").val()); 	
+        $(".main").removeClass("hidden");
+        $(".file-screen").addClass("hidden");
+        $(".show-log").attr("readonly","false");
+		return false;
+	});
+    $("#log-form").submit(function(event){
+        writeEntry();
+        return false;
+    });
+    function createLogFile(){
+        openFile = $("#filename").val() + ".log";
+		//alert(openFile);
+		filePath = "logfiles/"+openFile;
+		fs.open(filePath,"w", (err, fd) => {
+            // => null, <fd>
+			//fs.appendFile(fd, "hallo");
+			fs.close(fd);
+		});
+    }
+
+    function writeEntry(){
+        entry_note = $("#log-msg").val(); 
+        filePath = "logfiles/"+openFile;
+		fs.open(filePath,"a+", (err, fd) => {
+  // => null, <fd>
+            fs.stat("/dir/file.txt", function(err, stats){
+                mtime = new Date(util.inspect(stats.mtime));
+                //atime = new Date(util.inspect(stats.atime));
+                //console.log(mtime);
+            });
+			fs.appendFile(fd,entry_num + "#" + "    " + entry_note + "(" + mtime + ")" + "\r\n");
+            entry_num++;
+			fs.close(fd);
+		});
+    }
+    
 });
 
